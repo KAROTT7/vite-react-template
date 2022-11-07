3ac3382bdf8c3eb2fc7b50a0ba0df8d60c816ba8
karott7 <karott7@gmail.com>
2022-11-07 15:51:02 +0800

    initialize

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import router from 'vite-plugin-react-views'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), router()]
+})
