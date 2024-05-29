8554513c2a90e9c1015d72d732924eedeedfb222
karott7 <karott7@gmail.com>
2024-05-29 15:20:42 +0800

    update tsconfig.json and eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,28 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  overrides: [
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
 			jsx: true
 		},
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
-  rules: {
-    '@typescript-eslint/no-unused-vars': 2,
-    'react/react-in-jsx-scope': 0,
-    'no-console': 2,
-  }
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		'@typescript-eslint/no-unused-vars': 2,
+		'react/react-in-jsx-scope': 0,
+		'no-console': 2,
+		'@typescript-eslint/no-non-null-assertion': 0
+	}
 }
