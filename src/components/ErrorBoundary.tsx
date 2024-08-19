dece31a1622913dd222ff43384461c29ece3b22c
karott7 <karott7@gmail.com>
2024-08-19 11:36:57 +0800

    update: ErrorBoundary 优化

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,7 +1,14 @@
 import { useRouteError } from 'react-router-dom'
 
 export default function ErrorBoundary() {
-	const error = useRouteError()
+	const error = useRouteError() as any
 
-	return <div>{JSON.stringify(error)}</div>
+	return (
+		<div className="p-2">
+			<div className="mb-2">Something was wrong!</div>
+			<div>
+				<pre>{error.stack || error.message}</pre>
+			</div>
+		</div>
+	)
 }
